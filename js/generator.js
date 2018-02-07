38c578b1b497219a47450bed4a58323945f50b08
Khoi Le <khoile@stanford.edu>
2018-02-07 11:28:13 -0800

    generator producing some sounds

--- js/generator.js
+++ js/generator.js
@@ -13,27 +13,58 @@ while(toProcess != ""){
     var pipeIndex = toProcess.indexOf("|");
     var pron = toProcess.substring(0, pipeIndex - 1);
     toProcess = toProcess.substring(pipeIndex + 2);
-    wordToPronunciation[word] = pron;
-    pronunciationToWord[pron] = word;
+    if(!wordToPronunciation[word]){
+      wordToPronunciation[word] = [pron];
+    }
+    else{
+      wordToPronunciation[word].push(word);
+    }
+    if(!pronunciationToWord[pron]){
+      pronunciationToWord[pron] = [word];
+    }
+    else
+    {
+      pronunciationToWord[pron].push(word);
+    }
     wordTrie.add(word);
     pronunciationTrie.add(pron);
 }
-//console.log(wordToPronunciation);
-console.log(pronunciationTrie);
+//console.log(pronunciationToWord);
+//console.log(pronunciationTrie);
 
 function findVal(){
   var searchText = document.getElementById("searchVal").value;
   console.log(searchText);
   console.log(wordTrie.contains(searchText));
   console.log(wordToPronunciation[searchText]);
-  var pron = wordToPronunciation[searchText];
+  var pron = wordToPronunciation[searchText][0];
+  var currentNode = pronunciationTrie.root;
+  var searchPron = "";
+  var soundsWithin = [];
+  console.log(currentNode);
   for(var i = 0; i < pron.length; i++){
-    for(nodeChild in pronunciationTrie.root.children){
+    var foundMatch = false;
+    for(nodeChild in currentNode.children){
       //console.log(nodeChild);
       if(pron[i] == nodeChild){
-        console.log(nodeChild);
+        console.log(currentNode.children[nodeChild]);
+        currentNode = currentNode.children[nodeChild];
+        if(currentNode.word != null && currentNode.word != ""){
+          soundsWithin.push(currentNode.word);
+          console.log("this is a word: " + currentNode.word);
+        }
+        searchPron += pron[i];
+        foundMatch = true;
         break;
       }
     }
+    // no nodeChild matched
+    if(!foundMatch) {
+      console.log("no matches found: alternates are: " + currentNode.children);
+    }
   }
+  for(var i = 0; i < soundsWithin.length; i++){
+    console.log(pronunciationToWord[soundsWithin[i]]);
+  }
+  console.log(searchPron);
 }
